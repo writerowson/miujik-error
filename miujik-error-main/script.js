e368a9a0a98a96711511d9a60c8f01a554dad116
coderowson <runurowson@gmail.com>
2022-03-09 22:02:08 +0600

    debug

--- miujik-error-main/script.js
+++ miujik-error-main/script.js
@@ -6,62 +6,66 @@ const elementById = (id) => {
 const handleSearch = () => {
   const keyword = elementById("keyword");
   const url = `https://theaudiodb.com/api/v1/json/2/search.php?s=${keyword.value}`;
-  console.log(url)
+  fetch(url)
     .then((res) => res.json())
     .then((data) => showArtists(data));
+  keyword.value = ""
 };
 
-const showArtists = (data) => {
-  const artistContainer = elementById("artist");
-  data?.artists?.forEach((artist) => {
+const showArtists = ({ artists }) => {
+  const artistContainer = elementById("artists");
+  console.log(artistContainer)
+  artists.forEach((artist) => {
     const div = document.createElement("div");
+    console.log(artist)
     div.classList.add("artist-card");
     div.innerHTML = `<div class="image-container">
     <div class="image-container-inner">
       <img
-        src="${artist.strArtistThumb}"
+        src="${artist.strArtistThumb ? artist.strArtistThumb : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQP3lC0SfgqCcTGipFh64hddM6xgBYQj90wOA&usqp=CAU"}"
         alt=""
       />
     </div>
   </div>
   <div class="info-container">
-    <h1>${artist.strArtist}</h1>
-    <p>Country: ${artist.strCountry}</p>
-    <p>Style: ${artist.strGenre}</p>
+    <h1>${artist.strArtist ? artist.strArtist : "Not available"}</h1>
+    <p>Country: ${artist.strCountry ? artist.strCountry : "Not available"}</p>
+    <p>Style: ${artist.strGenre ? artist.strGenre : "Not available"}</p>
   </div>
-  <button class="album-button">
+  <button onclick="fetchAlbums('${artist.idArtist ? artist.idArtist : "Not available"}')"  class="album-button">
     <i class="fa-solid fa-compact-disc"></i>
-    <p onclick="fetchAlbums('${artist.idArtist}')" class="button-title">Albums</p>
+    <p class="button-title">Albums</p>
   </button>`;
     artistContainer.appendChild(div);
   });
 };
 
 const fetchAlbums = (id) => {
-  const url = `theaudiodb.com/api/v1/json/2/album.php?i=${id}`;
+  const url = `https://theaudiodb.com/api/v1/json/2/album.php?i=${id}`;
   fetch(url)
-    .then((res) => res.JSON())
-    .then((data) => showAlbum(data));
+    .then((res) => res.json())
+    .then((data) => showAlbum(data.album));
   const artistContainer = elementById("artists");
   artistContainer.innerHTML = "";
+
 };
 
 const showAlbum = (data) => {
   const albumContainer = elementById("albums");
-  album.forEach((item) => {
+  data.forEach((album) => {
     const div = document.createElement("div");
     div.classList.add("album");
     div.innerHTML = `
-        <div class="album-image-container">
-          <img
-            src="${album.strAlbumThumb}"
-            alt=""
-          />
-        </div>
-        <div class="album-name">
-          <h3>${album.strAlbum}</h3>
-        </div>
-      `;
+    //       <div class="album-image-container">
+    //         <img
+    //           src="${album.strAlbumThumb}"
+    //           alt=""
+    //         />
+    //       </div>
+    //       <div class="album-name">
+    //         <h3>${album.strAlbum}</h3>
+    //       </div>
+    //     `;
 
     albumContainer.appendChild(div);
   });
